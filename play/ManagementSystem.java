98749fdff18a3f4cc04684e35e7d252396615411
naim-ahammed <125636794+naim-ahammed@users.noreply.github.com>
2023-06-02 00:02:58 +0600

    3rd

---
+++ play/ManagementSystem.java
@@ -0,0 +1,8 @@
+package play;
+
+public class ManagementSystem {
+    public static void main(String[] args) {
+        RandomNumberGame game = new RandomNumberGame();
+        game.play();
+    }
+}
